f8fc6ea768d65c7772d7485761135e3939c31f65
Jasonksj <106599714+Jasonksj@users.noreply.github.com>
2022-10-13 20:37:12 +0100

    Delete Command.sql

--- Sweet World/Tables/Command.sql
+++
@@ -1,8 +0,0 @@
-Prompt ***** Creating COMMANDE table.....
-create table COMMANDE (
-    N°cmd       integer(10)      not null,
-    DateCmd      date            not null,
-    id_plat integer(10)    not null,
-    mode           integer(10)   not null,
-
-);
