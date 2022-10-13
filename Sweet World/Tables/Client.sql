c5a88cafcf576ed573c0683ca27e4ca99345bcff
nilisofficiel <versselsiyapze@gmail.com>
2022-10-13 19:49:13 +0200

    creation de la table client et commande

---
+++ Sweet World/Tables/Client.sql
@@ -0,0 +1,11 @@
+Prompt ******** Creating CLIENTS TABLE....... 
+create table CLIENTS (
+    id_client            integer(10)        not null,
+    Nom                  varchar(255)       not null,
+    Prenom               varchar(255)       not null,
+    Telephone            integer(10)        not null,
+    Email                varchar(255)       not null,
+    
+);
+
+  
---
+++ Sweet World/Tables/Command.sql
@@ -0,0 +1,8 @@
+Prompt ***** Creating COMMANDE table.....
+create table COMMANDE (
+    N°cmd       integer(10)      not null,
+    DateCmd      date            not null,
+    id_plat integer(10)    not null,
+    mode           integer(10)   not null,
+
+);
