505a0f92eab6daee40c88ba4cc5b91bbfeb16f6c
Tatsinkou <tatsinkoucrispin46@gmail.com>
2022-10-18 18:23:18 +0100

    modification de la table restaurant

--- Sweet World/Tables/table_restaurant.sql
+++ Sweet World/Tables/table_restaurant.sql
@@ -2,6 +2,8 @@ CREATE TABLE restaurants
     (id_restau   INTEGER(10)NOT NULL,
       nom_restau VARCHAR(255) NOT NULL,
       description VARCHAR(255) ,
-      nom_employe  VARCHAR2(255) NOT NULL        
+      nom_employe  VARCHAR2(255) NOT NULL,
+      latitude NUMBER(10, 3),
+      longitude NUMBER(10, 3)        
     );
     --***********BY TATSINKOU**********--
