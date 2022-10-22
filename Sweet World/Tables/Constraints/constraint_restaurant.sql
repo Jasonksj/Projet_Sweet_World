f9a01d58f73c9f654086807fd9998a5c6edf52c5
Jasonkjs <jasonkamsu@gmail.com>
2022-10-22 13:22:18 +0100

    Petit ajustement

--- Sweet World/Tables/Constraints/constraint_restaurant.sql
+++ Sweet World/Tables/Constraints/constraint_restaurant.sql
@@ -6,10 +6,10 @@ ADD
 	CONSTRAINT fk_restaurant
         FOREIGN KEY (id_order)
         REFERENCES Orders(id_order),
-    CONSTRAINT Location_add_fk
+    CONSTRAINT Location_fk
 		FOREIGN KEY (latitude, longitude)
 		REFERENCES locations(latitude, longitude),
-	CONSTRAINT Roles_add_fk
+	CONSTRAINT Roles_fk
 		FOREIGN KEY (id_role)
 		REFERENCES Roles(id_role)
 ) ;
