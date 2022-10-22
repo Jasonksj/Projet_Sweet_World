7b7c1521627a616a4b479e5b146f9e3e3b672cf3
Jasonkjs <jasonkamsu@gmail.com>
2022-10-22 16:03:23 +0100

    Petit ajustement

--- Sweet World/Datas/Datas_category.sql
+++ Sweet World/Datas/Datas_category.sql
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-
-
 INSERT INTO Category (id_category,Name_cat) VALUES(1,"Fast food");
 INSERT INTO Category (id_category,Name_cat) VALUES(2,"Grillade");
 INSERT INTO Category (id_category,Name_cat) VALUES(3,"Salades");
@@ -9,8 +5,3 @@ INSERT INTO Category (id_category,Name_cat) VALUES(4,"Pizza");
 INSERT INTO Category (id_category,Name_cat) VALUES(5,"Menu traditionnel");
 INSERT INTO Category (id_category,Name_cat) VALUES(6,"Dessert");
 INSERT INTO Category (id_category,Name_cat) VALUES(7,"Boisson");
-
-=======
-INSERT
-INTO Category (id_category,Name_cat) VALUES(1,"Fast food");
->>>>>>> f3b9ace1d6e0668df78271c4179586aacb2ab308
