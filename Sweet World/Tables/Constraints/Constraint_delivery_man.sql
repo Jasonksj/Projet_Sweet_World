acaff855b6864f076cba93f7b8cd54aced155f22
manuelfotso <manuelfotso93@gmail.com>
2022-10-23 10:48:02 +0200

    ajustement

--- Sweet World/Tables/Constraints/Constraint_delivery_man.sql
+++ Sweet World/Tables/Constraints/Constraint_delivery_man.sql
@@ -1,12 +1,11 @@
 ALTER TABLE Delivery_man
-ADD 
-    CONSTRAINT Delivery_man_pk
-        PRIMARY KEY (id_order);
-
-ALTER TABLE Delivery_man
-ADD 
-    CONSTRAINT fk_Delivery_man
-        FOREIGN KEY (id_order)
-        REFERENCES Orders(id_order);
+ADD
+(
+    CONSTRAINT id_deliv_man_pk
+        PRIMARY KEY(id_deliv_man),
+    CONSTRAINT fk_id_order
+        FOREIGN KEY(id_order)
+        REFERENCES Orders (id_order)
+);
 
     ----***** by FOTSO ******----
