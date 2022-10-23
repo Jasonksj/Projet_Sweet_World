e2fd2924a7deb911a685474fc4b9c316e77bf267
Jasonkjs <jasonkamsu@gmail.com>
2022-10-23 20:56:27 +0100

    Mise a jour Run fonctionnel

--- Sweet World/Tables/Constraints/Constraint_delivery_man.sql
+++ Sweet World/Tables/Constraints/Constraint_delivery_man.sql
@@ -1,17 +1,5 @@
 ALTER TABLE Delivery_man
-<<<<<<< HEAD
 ADD 
     CONSTRAINT Delivery_man_pk
         PRIMARY KEY (id_deliv_man);
-=======
-ADD
-(
-    CONSTRAINT id_deliv_man_pk
-        PRIMARY KEY(id_deliv_man),
-    CONSTRAINT fk_id_order
-        FOREIGN KEY(id_order)
-        REFERENCES Orders (id_order)
-);
-
->>>>>>> acaff855b6864f076cba93f7b8cd54aced155f22
     ----***** by FOTSO ******----
