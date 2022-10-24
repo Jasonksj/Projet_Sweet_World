72d3d837e097975d1278486c813f94707d602b70
manuelfotso <manuelfotso93@gmail.com>
2022-10-24 22:04:54 +0200

    commande

--- Sweet World/Datas/Datas_orders.sql
+++ Sweet World/Datas/Datas_orders.sql
@@ -1,42 +1,128 @@
-INSERT INTO Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0001,TO_DATE('02/01/2022', 'DD/MM/YYYY'),1,1,1,1);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0002,TO_DATE('02/01/2022', 'DD/MM/YYYY'),2,2,2,2);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0003,TO_DATE('02/01/2022', 'DD/MM/YYYY'),3,3,3,3);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0004,TO_DATE('06/02/2022', 'DD/MM/YYYY'),10,4,4,4);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0005,TO_DATE('06/02/2022', 'DD/MM/YYYY'),6,5,5,5);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0006,TO_DATE('18/02/2022', 'DD/MM/YYYY'),5,6,6,6);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0007,TO_DATE('18/03/2022', 'DD/MM/YYYY'),2,7,7,7);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0008,TO_DATE('02/03/2022', 'DD/MM/YYYY'),4,8,8,8);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0009,TO_DATE('18/04/2022', 'DD/MM/YYYY'),1,9,9,9);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0010,TO_DATE('12/04/2022', 'DD/MM/YYYY'),4,10,10,10);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0011,TO_DATE('16/04/2022', 'DD/MM/YYYY'),3,11,11,11);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0012,TO_DATE('23/04/2022', 'DD/MM/YYYY'),2,12,12,12);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0013,TO_DATE('14/04/2022', 'DD/MM/YYYY'),5,13,13,13);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0014,TO_DATE('19/04/2022', 'DD/MM/YYYY'),7,14,14,14);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0015,TO_DATE('20/04/2022', 'DD/MM/YYYY'),8,15,15,15);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0016,TO_DATE('10/04/2022', 'DD/MM/YYYY'),9,16,16,16);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0017,TO_DATE('25/05/2022', 'DD/MM/YYYY'),1,17,17,17);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0018,TO_DATE('30/05/2022', 'DD/MM/YYYY'),2,18,18,18);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0019,TO_DATE('14/05/2022', 'DD/MM/YYYY'),3,19,19,19);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0020,TO_DATE('29/05/2022', 'DD/MM/YYYY'),1,20,20,20);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0021,TO_DATE('31/06/2022', 'DD/MM/YYYY'),2,21,21,21);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0022,TO_DATE('18/06/2022', 'DD/MM/YYYY'),3,22,22,22);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0023,TO_DATE('17/06/2022', 'DD/MM/YYYY'),4,23,23,23);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0024,TO_DATE('19/06/2022', 'DD/MM/YYYY'),4,24,24,24);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0025,TO_DATE('20/06/2022', 'DD/MM/YYYY'),5,25,25,25);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0026,TO_DATE('02/06/2022', 'DD/MM/YYYY'),1,26,26,26);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0027,TO_DATE('19/07/2022', 'DD/MM/YYYY'),10,27,27,27);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0028,TO_DATE('02/07/2022', 'DD/MM/YYYY'),9,28,28,28);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0029,TO_DATE('16/08/2022', 'DD/MM/YYYY'),8,29,29,29);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0030,TO_DATE('12/08/2022', 'DD/MM/YYYY'),6,30,30,30);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0031,TO_DATE('12/08/2022', 'DD/MM/YYYY'),6,31,31,31);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0032,TO_DATE('12/08/2022', 'DD/MM/YYYY'),1,32,32,32);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0033,TO_DATE('12/09/2022', 'DD/MM/YYYY'),2,33,33,33);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0034,TO_DATE('12/09/2022', 'DD/MM/YYYY'),8,34,34,34);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0035,TO_DATE('12/10/2022', 'DD/MM/YYYY'),7,35,35,35);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0036,TO_DATE('10/10/2022', 'DD/MM/YYYY'),9,36,36,36);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0037,TO_DATE('09/10/2022', 'DD/MM/YYYY'),10,37,37,37);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0038,TO_DATE('12/10/2022', 'DD/MM/YYYY'),2,38,38,38);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0039,TO_DATE('10/10/2022', 'DD/MM/YYYY'),9,39,39,39);
-INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0040,TO_DATE('11/10/2022', 'DD/MM/YYYY'),6,40,40,40);
+---client 1---
+
+INSERT INTO Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0001,TO_DATE('02/01/2019', 'DD/MM/YYYY'),1,1,1,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0042,TO_DATE('20/11/2019', 'DD/MM/YYYY'),1,2,2,3);
+----client 2-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0005,TO_DATE('03/01/2019', 'DD/MM/YYYY'),2,3,3,38);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0010,TO_DATE('20/01/2019', 'DD/MM/YYYY'),2,4,4,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0015,TO_DATE('11/02/2019', 'DD/MM/YYYY'),2,5,5,5);
+----client 3 ----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0006,TO_DATE('06/01/2019', 'DD/MM/YYYY'),3,6,6,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0012,TO_DATE('20/01/2019', 'DD/MM/YYYY'),3,7,7,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0014,TO_DATE('11/02/2019', 'DD/MM/YYYY'),3,8,8,2);
+----client 4 ----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0004,TO_DATE('03/01/2019', 'DD/MM/YYYY'),4,9,9,1);
+----client 5----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0007,TO_DATE('06/01/2019', 'DD/MM/YYYY'),5,10,10,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0011,TO_DATE('20/01/2019', 'DD/MM/YYYY'),5,11,11,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0016,TO_DATE('20/02/2019', 'DD/MM/YYYY'),5,12,12,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0020,TO_DATE('20/02/2019', 'DD/MM/YYYY'),5,13,13,3);
+ ------client 6 -----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0003,TO_DATE('02/01/2019', 'DD/MM/YYYY'),6,14,14,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0008,TO_DATE('08/01/2019', 'DD/MM/YYYY'),6,15,15,5);
+-------client 7 --------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0009,TO_DATE('10/04/2019', 'DD/MM/YYYY'),7,16,16,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0017,TO_DATE('20/02/2019', 'DD/MM/YYYY'),7,17,17,1);
+-----client 8 ----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0018,TO_DATE('20/02/2019', 'DD/MM/YYYY'),8,18,18,2);
+-----client 9-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0019,TO_DATE('20/02/2019', 'DD/MM/YYYY'),9,19,19,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0002,TO_DATE('02/01/2019', 'DD/MM/YYYY'),9,20,20,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0013,TO_DATE('11/02/2019', 'DD/MM/YYYY'),9,21,21,5);
+------client 10-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0030,TO_DATE('06/06/2019', 'DD/MM/YYYY'),10,22,22,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0032,TO_DATE('06/06/2019', 'DD/MM/YYYY'),10,23,23,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0039,TO_DATE('12/09/2019', 'DD/MM/YYYY'),10,24,24,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0043,TO_DATE('20/11/2019', 'DD/MM/YYYY'),10,25,25,5);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0049,TO_DATE('10/01/2020', 'DD/MM/YYYY'),10,26,26,2);
+----- client 11-------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0029,TO_DATE('06/06/2019', 'DD/MM/YYYY'),11,27,27,2);
+--------- client 12 -----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0047,TO_DATE('10/12/2019', 'DD/MM/YYYY'),12,28,28,2);
+-------client 13-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0023,TO_DATE('03/03/2019', 'DD/MM/YYYY'),13,29,29,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0025,TO_DATE('04/04/2019', 'DD/MM/YYYY'),13,30,30,3);
+----- client 14 ----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0021,TO_DATE('03/03/2019', 'DD/MM/YYYY'),14,31,31,1);
+------ client 15 -----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0040,TO_DATE('12/09/2019', 'DD/MM/YYYY'),15,32,32,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0050,TO_DATE('10/01/2020', 'DD/MM/YYYY'),15,33,33,3);
+-----client 17 -----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0034,TO_DATE('15/07/2019', 'DD/MM/YYYY'),17,34,34,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0036,TO_DATE('15/07/2019', 'DD/MM/YYYY'),17,35,35,5);
+----- client 18 --------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0033,TO_DATE('15/07/2019', 'DD/MM/YYYY'),18,36,36,3);
+------client 19 ------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0022,TO_DATE('03/03/2019', 'DD/MM/YYYY'),19,37,37,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0038,TO_DATE('12/09/2019', 'DD/MM/YYYY'),19,38,38,3);
+---- client 20 ------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0035,TO_DATE('15/07/2019', 'DD/MM/YYYY'),20,39,39,3);
+ ----client 21 ------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0027,TO_DATE('04/04/2019', 'DD/MM/YYYY'),21,40,40,4);
+----- client 22----- 
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0024,TO_DATE('03/03/2019', 'DD/MM/YYYY'),22,18,18,1);
+-----client 23-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0026,TO_DATE('04/04/2019', 'DD/MM/YYYY'),23,19,19,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0028,TO_DATE('06/06/2019', 'DD/MM/YYYY'),23,20,20,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0031,TO_DATE('06/06/2019', 'DD/MM/YYYY'),23,21,21,1);
+------client 24-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0037,TO_DATE('12/09/2019', 'DD/MM/YYYY'),24,22,22,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0041,TO_DATE('20/11/2019', 'DD/MM/YYYY'),24,23,23,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0044,TO_DATE('10/12/2019', 'DD/MM/YYYY'),24,24,24,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0051,TO_DATE('10/01/2020', 'DD/MM/YYYY'),24,25,25,5);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0052,TO_DATE('10/01/2020', 'DD/MM/YYYY'),24,26,26,2);
+----- client 25-------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0054,TO_DATE('15/02/2020', 'DD/MM/YYYY'),25,27,27,2);
+--------- client 26 -----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0045,TO_DATE('10/12/2019', 'DD/MM/YYYY'),26,28,28,2);
+-------client 28-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0046,TO_DATE('10/12/2019', 'DD/MM/YYYY'),28,29,29,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0048,TO_DATE('10/01/2020', 'DD/MM/YYYY'),28,30,30,3);
+----- client 29 ----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0053,TO_DATE('15/02/2020', 'DD/MM/YYYY'),29,31,31,3);
+------ client 30 -----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0070,TO_DATE('10/08/2021', 'DD/MM/YYYY'),30,32,32,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0073,TO_DATE('12/11/2021', 'DD/MM/YYYY'),30,33,33,3);
+-----client 31 -----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0056,TO_DATE('20/04/2020', 'DD/MM/YYYY'),31,34,34,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0060,TO_DATE('10/06/2020', 'DD/MM/YYYY'),31,35,35,5);
+----- client 32 --------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0055,TO_DATE('15/02/2020', 'DD/MM/YYYY'),32,36,36,3);
+------client 33 ------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0057,TO_DATE('20/04/2020', 'DD/MM/YYYY'),33,37,37,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0058,TO_DATE('20/04/2020', 'DD/MM/YYYY'),33,38,38,3);
+---- client 34 ------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0059,TO_DATE('20/04/2020', 'DD/MM/YYYY'),34,39,39,3);
+ ----client 35 ------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0061,TO_DATE('10/06/2020', 'DD/MM/YYYY'),35,40,40,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0063,TO_DATE('10/06/2020', 'DD/MM/YYYY'),35,18,18,1);
+-----client 36-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0071,TO_DATE('10/00/2021', 'DD/MM/YYYY'),36,19,19,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0076,TO_DATE('12/01/2022', 'DD/MM/YYYY'),36,20,20,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0078,TO_DATE('12/01/2022', 'DD/MM/YYYY'),36,21,21,2);
+------client 37-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0062,TO_DATE('10/06/2020', 'DD/MM/YYYY'),37,22,22,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0064,TO_DATE('09/04/2021', 'DD/MM/YYYY'),37,23,23,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0067,TO_DATE('09/04/2021', 'DD/MM/YYYY'),37,24,24,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0069,TO_DATE('10/08/2021', 'DD/MM/YYYY'),37,25,25,5);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0072,TO_DATE('12/11/2021', 'DD/MM/YYYY'),37,26,26,2);
+-------client 38-----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0065,TO_DATE('09/04/2021', 'DD/MM/YYYY'),38,29,29,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0066,TO_DATE('09/04/2021', 'DD/MM/YYYY'),38,30,30,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0074,TO_DATE('12/11/2021', 'DD/MM/YYYY'),38,31,31,1);
+------ client 39 -----
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0068,TO_DATE('10/08/2021', 'DD/MM/YYYY'),39,32,32,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0075,TO_DATE('12/11/2021', 'DD/MM/YYYY'),39,33,33,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0077,TO_DATE('12/01/2022', 'DD/MM/YYYY'),39,34,34,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0081,TO_DATE('14/02/2022', 'DD/MM/YYYY'),39,35,35,5);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0084,TO_DATE('10/03/2022', 'DD/MM/YYYY'),39,36,36,3);
+------client 40 ------
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0079,TO_DATE('14/02/2022', 'DD/MM/YYYY'),40,37,37,3);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0080,TO_DATE('14/02/2022', 'DD/MM/YYYY'),40,38,38,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0082,TO_DATE('10/03/2022', 'DD/MM/YYYY'),40,39,39,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0083,TO_DATE('10/03/2022', 'DD/MM/YYYY'),40,40,40,4);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0085,TO_DATE('20/05/2022', 'DD/MM/YYYY'),40,38,38,1);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0086,TO_DATE('10/07/2022', 'DD/MM/YYYY'),40,39,39,2);
+INSERT INTO  Orders (id_order,order_date,quantity,id_cust,id_meal,id_payment) VALUES (0087,TO_DATE('10/10/2022', 'DD/MM/YYYY'),40,40,40,4);
 
 -----****by fotso ***---
--- Sweet World/Tables/Constraints/Constraint_delivery_man.sql
+++ Sweet World/Tables/Constraints/Constraint_delivery_man.sql
@@ -5,7 +5,7 @@ ADD
         PRIMARY KEY(id_deliv_man),
     CONSTRAINT fk_id_order
         FOREIGN KEY(id_order)
-        REFERENCES Orders (id_order)
+        REFERENCES Orders(id_order)
 );
 
     ----***** by FOTSO ******----
