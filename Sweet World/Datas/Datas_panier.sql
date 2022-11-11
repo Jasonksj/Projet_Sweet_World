5ecca793efea788433a8409007886b0769b65b52
manuelfotso <manuelfotso93@gmail.com>
2022-11-11 18:44:53 +0100

    ajout des insertions

---
+++ Sweet World/Datas/Datas_panier.sql
@@ -0,0 +1,18 @@
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0001,1,0001);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0002,2,0002);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0003,3,0003);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0004,4,0004);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0005,5,0005);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0006,6,0006);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0007,7,0007);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0008,8,0008);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0009,9,0009);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0010,10,0010);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0011,11,0011);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0012,12,0012);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0013,13,0013);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0014,14,0014);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0015,15,0015);
+INSERT INTO Panier(id_panier,id_cust,id_menu)VALUES(0016,16,0016);
+
+********** BY FOTSO *************
