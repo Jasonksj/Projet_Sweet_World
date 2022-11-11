eede74b4d27782bb7c7d4718319d0bc5b1734ad4
Samy bodio <115032144+Samy-Bodio@users.noreply.github.com>
2022-11-11 17:29:06 +0100

    Modification des datas customers

--- Sweet World/Datas/Datas_customers.sql
+++ Sweet World/Datas/Datas_customers.sql
@@ -1,118 +1,45 @@
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(1,'samyBh','Samy','Bodio',690239771,'samybodio2@gmail.com',TO_DATE('01/01/2019', 'DD/MM/YYYY'),'samy23',1,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(1,"Samy","Bodio",690237771,"samybodio2@gmail.com",TO_DATE('01/01/2019', 'DD/MM/YYYY'),"Douala-Yassa",1)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(2,'BoumssongBh','Boumssong','Jean-Pierre',690642527,'boumssongjp@gmail.com',TO_DATE('05/08/2009', 'DD/MM/YYYY'),'dffgsdhfdh23',2,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(2,'Boumssong','Aristide',690642527,'boumssongjp@gmail.com',TO_DATE('05/08/2009', 'DD/MM/YYYY'),"Douala-Yassa",2)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(3,'NtanguenBh','Ntanguen','Evrad',690020305,'evradntanguen@gmail.com',TO_DATE('07/12/2019', 'DD/MM/YYYY'),'Pfeyf13',3,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(3,'Rolande','Ngo Bong',690825412,'rolandeNgo@gmail.com',TO_DATE('05/04/1995', 'DD/MM/YYYY'),"Douala-Bonaberi",3)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(4,'KenchoungBh','Kenchoung','Sadio',699999999,'danySadio@gmail.com',TO_DATE('08/07/2020', 'DD/MM/YYYY'),'fgsydf823',4,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(4,'Juliette','Valdes',690630204,'Valdine@gmail.com',TO_DATE('29/03/2000', 'DD/MM/YYYY'),"Douala-Newbell",4)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(5,'AbdelBh','Abdel','Nabil',690858123,'abdelnabil@gmail.com',TO_DATE('01/10/2016', 'DD/MM/YYYY'),'sgduffg783',5,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(5,'Jason','Kamsu',690230458,'jjk@gmail.com',TO_DATE('15/18/2019', 'DD/MM/YYYY'),"village",5)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(6,'NjoyaBh','Njoya','Fadil',676023516,'Njoyafadil@gmail.com',TO_DATE('02/01/2019', 'DD/MM/YYYY'),'fdudfvi8',6,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(6,'Anaelle','Makolo',690642527,'EnaelleMkolo@gmail.com',TO_DATE('17/03/1996', 'DD/MM/YYYY'),"Newbell-Bassa",6)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(7,'OumarouBh','Oumarou','Adamou',693050296,'Oumarouadamou@gmail.com',TO_DATE('08/12/2018', 'DD/MM/YYYY'),'fcusklf789',7,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(7,'Tatinkou','Crispin',698884127,'Tatinkou@gmail.com',TO_DATE('01/07/2011', 'DD/MM/YYYY'),"Logbessou",7)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(8,'AifoudaBh','Aifouda','Blama',690239881,'aifoudablama@gmail.com',TO_DATE('01/02/2018', 'DD/MM/YYYY'),'79ydrf',8,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(8,'Loic','Patric',697584747,'LoicPatric@gmail.com',TO_DATE('17/03/1996', 'DD/MM/YYYY'),"Bonamoussadi",8)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(9,'TedomBh','Tedom','Keurtis',676820203,'Tedomkeurtis@gmail.com',TO_DATE('12/11/2018', 'DD/MM/YYYY'),'fdyffg7',9,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(9,'Manuel','fotso',691232587,'ManuelFotso@gmail.com',TO_DATE('04/10/2006', 'DD/MM/YYYY'),"Tchang",9)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(10,'JasonJJK','Kamsu','Jason',693910055,'jasonkamsu@gmail.com',TO_DATE('06/06/2019', 'DD/MM/YYYY'),'syfufj85',10,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(10,'Alix','Audrey',697251527,'motsoalix@gmail.com',TO_DATE('17/03/2010', 'DD/MM/YYYY'),"Village",10)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(11,'SamuelBh','Samuel','Nathan',690186547,'samuelnathan2@gmail.com',TO_DATE('25/05/2021','DD/MM/YYYY'),'ydidj13',11,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(11,'Arnold','Tekam',694451512,'arnoldtekam@gmail.com',TO_DATE('09/01/2016', 'DD/MM/YYYY'),"Elvie",11)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(12,'DjidjouBh','Djidjou','Audrey',656642527,'djidjouaudrey@gmail.com',TO_DATE('03/04/2015','DD/MM/YYYY'),'qyraud12',12,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(12,'Eliane','Mimi',692541672,'elianemimi@gmail.com',TO_DATE('01/05/2015', 'DD/MM/YYYY'),"ngouakele",12)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(13,'NtanguenBh','Ntanguen','Evrad',690020305,'evradntanguen@gmail.com',TO_DATE('07/02/2019','DD/MM/YYYY'),'stdfs85',13,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(13,'Leslie','Ivanka',692464248,'LeslieIvanka@gmail.com',TO_DATE('21/08/2013', 'DD/MM/YYYY'),"hilton",13)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(14,'MbaBh','Mba','Makam',699984999,'MbaMakam@gmail.com',TO_DATE('08/07/2019','DD/MM/YYYY'),'16dff12',14,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
+(14,'Ermione','Artina',692525848,'ErmioneArt@gmail.com',TO_DATE('28/11/2012', 'DD/MM/YYYY'),"Deido",14)
+INSERT INTO Customers(id_cust,first_name,last_name,phone_number,Email,Datecd_cust,Cust_address,id_user)
 VALUES
-(15,'boloBh','bolo','valid',659858123,'bolovalid@gmail.com',TO_DATE('01/10/2015','DD/MM/YYYY'),'cndhd15fg',15,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(16,'joyceBh','joyce','nkound',65692516,'joycenkound@gmail.com',TO_DATE('06/08/2005','DD/MM/YYYY'),'15hahsfagf',16,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(17,'sLeonelBh','Leonel','David',693055974,'LeonelDavid@gmail.com',TO_DATE('08/02/2019','DD/MM/YYYY'),'faasdf15',17,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(18,'LeslieBh','Leslie','Ivanka',690859881,'LeslieIvanka@gmail.com',TO_DATE('01/02/2013','DD/MM/YYYY'),'dsgd921',18,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(19,'ArielBh','Ariel','valdes',676820203,'Arielvaldes@gmail.com',TO_DATE('04/04/2017','DD/MM/YYYY'),'7sgfgas',19,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(20,'StevenBh','Steven','Poala',690805060,'StevenPoala@gmail.com',TO_DATE('29/09/2018','DD/MM/YYYY'),'tdste9',20,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(21,'SamUELBh','SamUEL','VOLTA',676279771,'samUELVOLTA2@gmail.com',TO_DATE('09/03/2022', 'DD/MM/YYYY'),'jtydtd7',21,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(22,'RobinBh','Robin','Van persie',656452527,'robinvanpersie@gmail.com',TO_DATE('06/07/2019', 'DD/MM/YYYY'),'hcusfgdk25',22,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(23,'CristianoBh','Cristiano','ronaldo',690020305,'cristianoronaldo@gmail.com',TO_DATE('25/08/2010', 'DD/MM/YYYY'),'18dfgujs',23,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(24,'ManeBh','Mane','Sadio',699988899,'ManeSadio@gmail.com',TO_DATE('04/04/2019', 'DD/MM/YYYY'),'18sfdjh',24,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(25,'AlbertoBh','Alberto','del rio',6955558123,'albertodel@gmail.com',TO_DATE('01/07/2020', 'DD/MM/YYYY'),'188dsfd',25,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(26,'HarryBh','Harry','Kane',676039516,'Harryk@gmail.com',TO_DATE('06/02/2000', 'DD/MM/YYYY'),'fhffdd85',26,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(27,'karimBh','karim','benzema',693050000,'karimbenzema@gmail.com',TO_DATE('08/02/2003', 'DD/MM/YYYY'),'fsdfhgh18',27,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(28,'NovakBh','Novak','DJOKOVIC',675569881,'GOAT@gmail.com',TO_DATE('01/01/2010', 'DD/MM/YYYY'),'dsgfd15',28,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(29,'RaphaelBh','Raphael','Nadal',688820203,'rafa@gmail.com',TO_DATE('15/02/2020', 'DD/MM/YYYY'),'dhasfd15',29,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(30,'RogerBh','Roger','Federer',690805060,'federer@gmail.com',TO_DATE('29/07/2021', 'DD/MM/YYYY'),'185gayusg',30,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(31,'SamuelBh','Samuel','Bryan',690186547,'samuelbryan2@gmail.com',TO_DATE('15/11/2019','DD/MM/YYYY'),'adffs74',31,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(32,'AlineBh','Aline','Audrey',656642850,'alineaudrey@gmail.com',TO_DATE('03/02/2020','DD/MM/YYYY'),'13fyefgg',32,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(33,'AndreBh','Andre','Eden',690020305,'Adree25@gmail.com',TO_DATE('07/02/2009','DD/MM/YYYY'),'294shdjf',33,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(34,'MboloBh','Mbolo','Christelle',677784999,'christinembolo@gmail.com',TO_DATE('30/07/2020','DD/MM/YYYY'),'kfdfdd12',34,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(35,'valdesBh','valdes','patrice',679258713,'patricevaldes23@gmail.com',TO_DATE('08/05/2013','DD/MM/YYYY'),'yastfdast85',35,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(36,'AlixBh','Alix','Audrey',65772516,'alixaudrey@gmail.com',TO_DATE('01/01/2021','DD/MM/YYYY'),'3jffv85',36,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(37,'LeoBh','Leo','Messi',693055974,'LeonelMessi@gmail.com',TO_DATE('08/12/2019','DD/MM/YYYY'),'ydastfd48',37,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(38,'LeslieBh','Leslie','johnson',699889184,'Lesliejohnson@gmail.com',TO_DATE('01/04/2021','DD/MM/YYYY'),'hdvsghg14',38,2);
-INSERT INTO Customers(id_cust,username,first_name,last_name,phone_number,Email,Datecd_cust,password_cust,id_address,id_role)
-VALUES
-(39,'Ariel2Bh','Ariel2','valdes',676820203,'Arielvaldes@gmail.com',TO_DATE('10/08/2021','DD/MM/YYYY'),'ksfdt10',39,2);
-
+(15,'Martina','liliane',651464298,'Martinalili@gmail.com',TO_DATE('08/12/2003', 'DD/MM/YYYY'),"iracle",15)
