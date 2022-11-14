f1a65321be4fc8ee0c9b861a0669e902ed1ecd3d
manuelfotso <manuelfotso93@gmail.com>
2022-11-14 18:37:53 +0100

    modification

--- Sweet World/Tables/Constraints/Foreign_Key/Constraint_panier.sql
+++ Sweet World/Tables/Constraints/Foreign_Key/Constraint_panier.sql
@@ -4,11 +4,5 @@ ADD
 		FOREIGN KEY (id_cust)
 		REFERENCES Customers(id_cust);
 
-ALTER TABLE Panier
-ADD
-	CONSTRAINT fk_id_menu
-		FOREIGN KEY (id_menu)
-		REFERENCES Menu(id_menu);
-
 
 --***********BY FOTSO**********--
--- Sweet World/Tables/Table_panier.sql
+++ Sweet World/Tables/Table_panier.sql
@@ -9,8 +9,6 @@ SET ECHO OFF
 CREATE TABLE Panier
 (
     id_panier  INT          NOT NULL,
-    quantity   INT          NOT NULL,
-   id_cust     INT          NOT NULL,
-   id_menu     INT          NOT NULL
+   id_cust     INT          NOT NULL
 );
 /* Cree par Fotso */
