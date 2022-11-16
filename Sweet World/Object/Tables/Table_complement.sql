e60d772d8c6a317456691072b6456685ca0d85cd
Annaelle_Josee <annaellejoseemegane@gmail.com>
2022-11-16 19:03:39 +0100

    creation des sequences dish et complement

--- Sweet World/Object/Tables/Table_complement.sql
+++ Sweet World/Object/Tables/Table_complement.sql
@@ -10,6 +10,5 @@ CREATE TABLE Complement
 ( 
      id_complement    INT    NOT NULL,
      Name_complement  VARCHAR(40)  NOT NULL,
-     Qty_complement  VARCHAR(40)  NOT NULL,
 );
 --*********BY annaellejosee13*********--
