d933e307173aac08616ded1ccf7ef3cd8fa9b263
manuelfotso <manuelfotso93@gmail.com>
2022-11-16 19:09:20 +0100

    modification

--- Sweet World/Object/Tables/Table_complement.sql
+++ Sweet World/Object/Tables/Table_complement.sql
@@ -10,6 +10,6 @@ CREATE TABLE Complement
 ( 
      id_complement    INT    NOT NULL,
      Name_complement  VARCHAR(40)  NOT NULL,
-     Qty_complement  VARCHAR(40)  NOT NULL,
+     Qty_complement  VARCHAR(40)  NOT NULL
 );
 --*********BY annaellejosee13*********--
