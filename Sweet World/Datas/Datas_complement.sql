190d916bb7a4bbd94524ca892e574339cc53f517
Jasonksj <jasonkamsu@gmail.com>
2022-11-16 18:40:37 +0100

    modification

--- Sweet World/Datas/Datas_complement.sql
+++ Sweet World/Datas/Datas_complement.sql
@@ -1,18 +1,18 @@
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (1,'Riz',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (2,'Plantain mur',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (3,'Macabo',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (4,'Banane',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (5,'Patate',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (6,'Pommes de terre',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (7,'Manioc',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (8,'Igname',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (9,'Frites de plantains ',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (10,'Frites de pommes de terre',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (11,' Mignondo',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (12,' Baton de manioc',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (13,' Couscous mais ',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (14,' Couscous de manioc ',20);
-INSERT INTO Complement ( id_complement , Name_complement ,  Qty_complement) VALUES (15,' Tapioca',20);
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (1,'Riz');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (2,'Plantain mur');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (3,'Macabo');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (4,'Banane');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (5,'Patate');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (6,'Pommes de terre');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (7,'Manioc');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (8,'Igname');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (9,'Frites de plantains ');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (10,'Frites de pommes de terre');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (11,' Mignondo');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (12,' Baton de manioc');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (13,' Couscous mais ');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (14,' Couscous de manioc ');
+INSERT INTO Complement ( id_complement , Name_complement) VALUES (15,' Tapioca');
 
 --*********BY annaellejosee13*********-- 
 
