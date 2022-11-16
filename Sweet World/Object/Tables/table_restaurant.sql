064b01bb9866ad83636b539ea4f91b4747331d02
Tatsinkou <tatsinkoucrispin46@gmail.com>
2022-11-16 18:59:33 +0100

    +

--- Sweet World/Object/Tables/table_restaurant.sql
+++ Sweet World/Object/Tables/table_restaurant.sql
@@ -12,7 +12,7 @@ CREATE TABLE Restaurant
       name_resto       VARCHAR(30)   NOT NULL,
       Description      VARCHAR(40),
       name_employee    VARCHAR(20)   NOT NULL,
-      phone_number     INT(9)        NOT NULL,
+      phone_number     INT       NOT NULL,
       resto_address    VARCHAR(50)   NOT NULL,
       Date_res_created DATE          NOT NULL,
       id_note          INT           NOT NULL,  
