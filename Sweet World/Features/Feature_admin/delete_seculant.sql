7b0d4e721443f44864bf51aefa06ab4f144a126b
annaellejosee13 <115103700+annaellejosee13@users.noreply.github.com>
2022-11-18 15:53:14 +0100

    Update delete_seculant.sql

--- Sweet World/Features/Feature_admin/delete_seculant.sql
+++ Sweet World/Features/Feature_admin/delete_seculant.sql
@@ -1,13 +1,6 @@
 DELETE FROM Seculant
 WHERE id_seculant = &id_seculant;
 
-SET DEFINE ON    
-BEGIN
-DELETE * FROM Seculant
-WHERE id_seculant => &id_seculant 
-END;
-/
-
 BEGIN
 sp_register (p_id_user => &id_user.nextVal,
              p_user_name => &username,   
